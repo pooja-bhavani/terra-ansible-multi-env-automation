bce977d0cbb0735953fd2073a5dfbe0a4c960488
Pooja bhavani <147735975+pooja-bhavani@users.noreply.github.com>
2026-07-18 13:50:46 +0530

    Add TLS provider and S3 backend configuration
    Added TLS provider and configured S3 backend for state management.

--- terraform.tf
+++ terraform.tf
@@ -1,9 +1,21 @@
 terraform {
-  required_version = ">= 1.5.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
+    required_version = ">= 1.5.0"
+    required_providers {
+      aws = {
+        source  = "hashicorp/aws"
+        version = "~> 6.0"
+      }
+      tls = {
+        source  = "hashicorp/tls"
+        version = "~> 4.0"
+      }
+    }
+
+    backend "s3" {
+      bucket         = "tfstate-multienv-2026"
+      key            = "infra/terraform.tfstate"
+      region         = "ap-south-1"
+      dynamodb_table = "tfstate-multienv-statelock"
+      encrypt        = true
     }
   }
-}
